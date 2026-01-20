b102cb08e23de4eb0229c24d58125d05d5c81308
AFREEN KHATOON <afreen.khatoon11122004@gmail.com>
2026-01-20 23:12:29 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -9,3 +9,7 @@ print("Addition:",addition)
 print("subtraction:",subtraction)
 print("multiplication:",multiplication)
 print("division:",division)
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Hello, " + full_name + "! Nice to meet you.")
